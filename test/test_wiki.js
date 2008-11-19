26d27ed8fe5f4be06a8e9009388079dfbd7dc7fc
Ethan Glasser-Camp <ethan@10gen.com>
2008-11-19 16:22:27 -0500

    Tests that won't work due to broken testing framework.

--- test/test_wiki.js
+++ test/test_wiki.js
@@ -1,12 +1,12 @@
 /**
 *      Copyright (C) 2008 10gen Inc.
-*  
+*
 *    Licensed under the Apache License, Version 2.0 (the "License");
 *    you may not use this file except in compliance with the License.
 *    You may obtain a copy of the License at
-*  
+*
 *       http://www.apache.org/licenses/LICENSE-2.0
-*  
+*
 *    Unless required by applicable law or agreed to in writing, software
 *    distributed under the License is distributed on an "AS IS" BASIS,
 *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -69,3 +69,15 @@ assert(o == "this is <code>some code</code>\n");
 s = "this is 100% pure code";
 o = w.toHtml(s);
 assert(o == "this is 100% pure code\n");
+
+return;
+
+// Tests won't work, because they run against wiki "stable"
+s = "=hi there=#some-anchor\n";
+o = w.toHtml(s);
+assert.eq(o, "<h1 id=\"some-anchor\">hi there</h1>\n");
+
+// FIXME: this might be a bug instead of a feature, but it's existing behavior
+s = "=hi there=blah blah";
+o = w.toHtml(s);
+assert.eq(o, "<h1>hi there</h1>blah blah\n");
