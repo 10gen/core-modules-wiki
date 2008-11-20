5fec8af035350d47b454c5142d543d333c1ee159
Ethan Glasser-Camp <ethan@10gen.com>
2008-11-20 10:29:41 -0500

    Strip anchor from location on edit.

--- assets/clientedit.js
+++ assets/clientedit.js
@@ -1,12 +1,12 @@
 /**
 *      Copyright (C) 2008 10gen Inc.
-*  
+*
 *    Licensed under the Apache License, Version 2.0 (the "License");
 *    you may not use this file except in compliance with the License.
 *    You may obtain a copy of the License at
-*  
+*
 *       http://www.apache.org/licenses/LICENSE-2.0
-*  
+*
 *    Unless required by applicable law or agreed to in writing, software
 *    distributed under the License is distributed on an "AS IS" BASIS,
 *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -118,7 +118,7 @@ clientEditLoader.insert({
 var handleEditKeyPress = function() {
     if(document.getElementById("searchtext") && textFocus) return;
     // redirect to edit page
-    window.location = window.location + "?edit=true";
+    window.location = window.location.toString().replace(/#.+$/, '') + "?edit=true";
 }
 
 var handleRenameKeyPress = function(e, obj) {
